dcb63764596d5d9a141e3b774c964ebf46a79881
saranshverma21345 <82048395+saranshverma21345@users.noreply.github.com>
2025-02-13 12:28:31 +0530

    Create db-config.php

---
+++ config/db-config.php
@@ -0,0 +1,14 @@
+<?php
+$host = "database-1.cdiee4q4s7k8.ap-south-1.rds.amazonaws.com";
+$username = "appdb";
+$password = "55B8kTYIARZHNjcbhyab";
+$dbname = "database-1";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+echo "Connected to RDS successfully!";
+?>
